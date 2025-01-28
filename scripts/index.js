d1df4b01c4929d4c18f5ee5d603dd23700cedc79
DARoyalIII <dudleyroyal@icloud.com>
2025-01-27 19:21:54 -0800

    Add Modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,4 +26,54 @@ const initialCards = [
   }
 ];
 
-console.log(initialCards);
+const profileEdit = document.querySelector(".profile__edit-button");
+const editModal = document.querySelector("#edit-modal");
+const closeModal = document.querySelector(".modal__close-btn");
+
+profileEdit.addEventListener("click", function () {
+  editModal.classList.add("modal_opened");
+});
+
+closeModal.addEventListener("click", function () {
+  editModal.classList.remove("modal_opened");
+});
+
+const profileName = document.querySelector(".profile__title");
+const profileSub = document.querySelector(".profile__subtitle");
+const editModalName = document.querySelector("#name");
+const editModalDescrip = document.querySelector("#description");
+
+editModalName.value = profileName.textContent;
+editModalDescrip.value = profileSub.textContent;
+
+const profileFormElement = document.querySelector(".modal__form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalName.value;
+  profileSub.textContent = editModalDescrip.value;
+  editModal.classList.remove("modal_opened");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list")
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+
+  const cardNameEle = cardElement.querySelector(".card__info");
+  const cardImgEle = cardElement.querySelector(".card__image");
+  cardImgEle.alt = data.name;
+
+  cardNameEle.textContent = data.name;
+  cardImgEle.src = data.link;
+
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.append(cardElement);
+}
