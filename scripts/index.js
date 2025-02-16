e19d1e4683f3d58d0355cae6ef1815a3424b9dd8
DARoyalIII <dudleyroyal@icloud.com>
2025-02-16 10:50:07 -0800

    Complete Modals function

--- scripts/index.js
+++ scripts/index.js
@@ -26,63 +26,104 @@ const initialCards = [
   }
 ];
 
+// Profile elements
 const profileEdit = document.querySelector(".profile__edit-button");
-const editModal = document.querySelector("#edit-modal");
-const closeModal = document.querySelector(".modal__close-btn");
-
 const profileName = document.querySelector(".profile__title");
 const profileSub = document.querySelector(".profile__subtitle");
-const editModalName = document.querySelector("#name");
-const editModalDescrip = document.querySelector("#description");
+const newPost = document.querySelector(".profile__newpost-button");
 
-const cardTemplate = document.querySelector("#card-template");
-const cardsList = document.querySelector(".cards__list")
+// Modal elements
+const editModal = document.querySelector("#edit-modal");
+const closeModal = editModal.querySelector(".modal__close-btn");
+const editModalName = editModal.querySelector("#name");
+const editModalDescrip = editModal.querySelector("#description");
 const profileFormElement = document.querySelector(".modal__form");
 const profileForm = document.forms["profile-form"];
 
-closeModal.addEventListener("click", exitModal);
+// Card elements
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list")
+
+
+// New post elements
+const postModal = document.querySelector("#post-modal-add");
+const postClose = postModal.querySelector(".modal__close-btn");
+const postFormElement = postModal.querySelector(".post-modal__form");
+const addImage = postModal.querySelector("#image");
+const addCaption = postModal.querySelector("#caption");
+
+// Profile edit modal events
+closeModal.addEventListener("click", () => {
+  exitModal(editModal);
+});
+
+profileEdit.addEventListener("click", () => {
+  editModalName.value = profileName.textContent;
+  editModalDescrip.value = profileSub.textContent;
+  openModal(editModal);
+});
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
-profileEdit.addEventListener("click", function () {
-  editModal.classList.add("modal_opened");
-  editModalName.value = profileName.textContent;
-  editModalDescrip.value = profileSub.textContent;
+// New post modal events
+newPost.addEventListener("click", () => {
+  openModal(postModal);
 });
 
+postClose.addEventListener("click", () => {
+  exitModal(postModal);
+})
 
-function exitModal() {
-  editModal.classList.remove("modal_opened");
+postFormElement.addEventListener("submit", handleNewPostSubmit);
+
+// Opening modals
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
+// Exiting moodals
+function exitModal(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+// Submitting updated profile data
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = editModalName.value;
   profileSub.textContent = editModalDescrip.value;
-  exitModal();
+  exitModal(editModal);
 }
 
+// Creating new cards and appending array data
 function getCardElement(data) {
   const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
-
   const cardNameEle = cardElement.querySelector(".card__info");
   const cardImgEle = cardElement.querySelector(".card__image");
   cardImgEle.alt = data.name;
-
   cardNameEle.textContent = data.name;
   cardImgEle.src = data.link;
-
   cardsList.append(cardElement);
-
   return cardElement;
 }
 
 initialCards.forEach(getCardElement);
 
+// Submitting new post data
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  addImage.value;
+  addCaption.value;
+  const newCard = { name: addCaption.value, link: addImage.value};
+  console.log(newCard);
+  const cardEl = getCardElement(newCard);
+  cardsList.prepend(cardEl);
+  addCardToArray(newCard);
+  exitModal(postModal);
+}
 
-
-
-
+function addCardToArray(item) {
+  initialCards.push(handleNewPostSubmit);
+}
 
 
 
